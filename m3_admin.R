3b56e968a8abadc6d80d37a4d87ea33683036c6b
juliette.engelaere <juliette.engelaere@developpement-durable.gouv.fr>
2025-09-12 18:10:06 +0200

    Ajout parties communes 1 et 2, ménage dans repo

--- m3_admin.R
+++ m3_admin.R
@@ -1,4 +1,8 @@
 # Commande pour générer à la main le book du module 3
-
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
+# Commande pour mettre à jour les dépendances dans DESCRIPTION
+# attachment::att_from_rmds(path = ".") # récupère la liste des packages utilisés dans le répertoire
+
+# impression_pdf avec pagedown chrome_print
+# propre.rpls::creer_pdf_book(pages_html = c("index.Rmd" list.files(pattern = "^0.*.Rmd"))
